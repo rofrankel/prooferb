991abfafc2750631779e30098c5fdd8e7663b084
Richard Frankel <richard@frankel.tv>
2011-04-17 23:37:40 -0400

    more nodester-related tweaking

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
  (function() {
   var app, prooferb;
   app = require('express').createServer();
-  // prooferb = require('./prooferb');
+  prooferb = require('./prooferb');
   app.get('/', function(req, res) {
     return res.send('Welcome to Prooferb.  Go to /a/b to generate a proof of the equivalence of \'a\' and \'b\'.<br><br>For example, visit <a href="/money/awesome">/money/awesome</a> to prove that money is awesome.');
   });

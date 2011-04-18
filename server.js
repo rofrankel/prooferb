100902a8286e0cedd05d67f9640ae65bfe1d4f57
Richard Frankel <richard@frankel.tv>
2011-04-17 23:37:22 -0400

    more nodester-related tweaking

--- server.js
+++ server.js
@@ -1,8 +1,7 @@
-/*
  (function() {
   var app, prooferb;
   app = require('express').createServer();
-  prooferb = require('./prooferb');
+  // prooferb = require('./prooferb');
   app.get('/', function(req, res) {
     return res.send('Welcome to Prooferb.  Go to /a/b to generate a proof of the equivalence of \'a\' and \'b\'.<br><br>For example, visit <a href="/money/awesome">/money/awesome</a> to prove that money is awesome.');
   });
@@ -19,22 +18,3 @@
   });
   app.listen(9511);
 }).call(this);
-*/
-
-
-var app, prooferb;
-app = require('express').createServer();
-prooferb = require('./prooferb.js');
-
-app.get('/', function(req, res) {
-  return res.send('Welcome to Prooferb.  Go to /a/b to generate a proof of the equivalence of \'a\' and \'b\'.<br><br>For example, visit <a href="/money/awesome">/money/awesome</a> to prove that money is awesome.');
-});
-app.listen(9511);
-
-/*
-var http = require('http');
-http.createServer(function (req, res) {
-  res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Hello World\nApp (rfrankel-test) is running..');
-}).listen(9511);
-*/
